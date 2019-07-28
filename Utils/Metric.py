6cef78835b9188edd213db26e560dac762f994b0
Jianfei2333 <xjf999999@hotmail.com>
2019-07-28 19:39:03 +0800

    add doc-mini entrancy

--- Utils/Metric.py
+++ Utils/Metric.py
@@ -1,6 +1,8 @@
 from prettytable import PrettyTable
 import numpy as np
 import pandas as pd
+import torch
+from ignite.utils import to_onehot
 
 def PrecisionRecallTable(p, r, cols):
   """
@@ -14,14 +16,10 @@ def PrecisionRecallTable(p, r, cols):
   Return:
     - table: PrettyTable object.
   """
-  # tab = PrettyTable()
   datatab = np.zeros((2, len(cols)+1))
-  # tab.add_column('Type', ['Precision', 'Recall'])
   for c in range(len(cols)):
-    # tab.add_column(cols[c], [round(p[c].item(), 4), round(r[c].item(), 4)])
     datatab[0, c] = round(p[c].item(), 4)
     datatab[1, c] = round(r[c].item(), 4)
-  # tab.add_column('Mean', [p.mean().item(), r.mean().item()])
   datatab[0, -1] = p.mean().item()
   datatab[1, -1] = r.mean().item()
   cols = np.hstack((np.array(cols), np.array(['mean'])))
@@ -42,16 +40,35 @@ def CMatrixTable(cmatrix, cols):
   Return:
     - table: PrettyTable object. Show the confusion matrix.
   """
-  # tab = PrettyTable()
   cmatrix = cmatrix.numpy().astype(int).T
-  # tab.add_column('gt\\pd', list(cols))
-  # for c in range(len(cols)):
-    # tab.add_column(cols[c], cmatrix[c])
   df = pd.DataFrame(cmatrix, columns=cols, index=cols)
-
-  # tab.border=False
-
   return {
     'pretty': df,
     'data': cmatrix
   }
+
+def Labels2Acc(labels):
+  y_pred, y_true = labels
+  accuracy = (y_pred == y_true).sum().item() / y_true.shape[0]
+  return accuracy
+
+def Labels2PrecisionRecall(labels, cols):
+  epsilon=1e-30
+  y_pred, y_true = labels
+  num_classes = len(cols)
+  y_pred = to_onehot(y_pred, num_classes)
+  y_true = to_onehot(y_true, num_classes)
+  tp = (y_pred * y_true).sum(0)
+  pred = y_pred.sum(0)
+  true = y_true.sum(0)
+  precision = tp/(pred+epsilon)
+  recall = tp/(true+epsilon)
+  return PrecisionRecallTable(precision, recall, cols)
+
+def Labels2CMatrix(labels, cols):
+  y_pred, y_true = labels
+  num_classes = len(cols)
+  cmatrix = torch.zeros(num_classes, num_classes)
+  for i in range(y_pred.shape(0)):
+    cmatrix[y_true[i].item()][y_pred[i].item()] += 1
+  return CMatrixTable(cmatrix, cols)
