b7d97263136e5091464ad6b9227ca7bf9e624174
Jianfei2333 <xjf999999@hotmail.com>
2019-07-30 16:11:34 +0800

    修改初始lr，使训练更稳定

--- Utils/Argparser.py
+++ Utils/Argparser.py
@@ -8,8 +8,8 @@ def GetArgParser():
                       help='input batch size for validation (default: 50)')
   parser.add_argument('-e', type=int, default= 200,
                       help='Number of epochs to train (default: 200)')
-  parser.add_argument('-lr', type=float, default= 3e-3,
-                      help='Learning rate (default: 3e-3)')
+  parser.add_argument('-lr', type=float, default= 1e-3,
+                      help='Learning rate (default: 1e-3)')
   parser.add_argument('--gpus', type=int, default=1,
                       help='Assign GPU numbers to use (default: 1)')
   parser.add_argument('--remark', type=str, default='debug',
